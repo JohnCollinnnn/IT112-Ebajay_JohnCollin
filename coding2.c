32df078f724bafaaa58803a913724dc7c0f7d130
John Collin Ebajay <ebajay.collin@gmail.com>
2026-08-16 22:22:31 +0800

    Update coding2 program

---
+++ coding2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("********************************************************\n");
+    printf("**                                                    **\n");
+    printf("**              *  *  *  *  *  *                      **\n");
+    printf("**           *                    *                   **\n");
+    printf("**        *      WELCOME, CODER!     *                **\n");
+    printf("**           *                    *                   **\n");
+    printf("**              *  *  *  *  *  *                      **\n");
+    printf("**                                                    **\n");
+    printf("**             C PROGRAMMING 1                        **\n");
+    printf("**                                                    **\n");
+    printf("**          * LEARN * CODE * CREATE *                 **\n");
+    printf("**                                                    **\n");
+    printf("**               KEEP CODING!                         **\n");
+    printf("**                                                    **\n");
+    printf("********************************************************\n");
+
+    return 0;
+}
